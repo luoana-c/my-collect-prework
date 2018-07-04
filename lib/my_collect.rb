d028af4cd686137d1aed0ea7928ce315206f9268
Luoana Chirita <luoana.livadariu@gmail.com>
2018-07-04 08:04:37 +0000

    collect

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,10 @@
-
-
+def my_collect(collection)
+  i = 0 
+  new_collection = []
+  while i < collection.length 
+    item = yield collection[i]
+    i = i + 1
+    new_collection.push(item)
+  end
+  return new_collection
+end
